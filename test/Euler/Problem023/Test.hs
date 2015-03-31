402f27a83ec25892b455e604bbc31875d4c856c3
Jason Whittle <jason.whittle@gmail.com>
2015-03-31 12:57:39 -0400

    Start using Template Haskell in test suites.

--- test/Euler/Problem023/Test.hs
+++ test/Euler/Problem023/Test.hs
@@ -1,20 +1,19 @@
+{-# OPTIONS_GHC -XTemplateHaskell #-}
+
 module Euler.Problem023.Test (suite) where
 
-import Test.Tasty (testGroup, TestTree)
+import Test.Tasty
 import Test.Tasty.HUnit
 import Test.Tasty.QuickCheck
+import Test.Tasty.TH
 
 import Euler.Problem023
 
 suite :: TestTree
-suite = testGroup "Problem023"
-        [ testCase "abundance of 12" test_abundanceOf12
-        , testCase "first 20 abundants" test_first20Abundants
-        , testProperty "sum of abundants" prop_sumOfAbundants
-        ]
+suite = $(testGroupGenerator)
 
-test_abundanceOf12 :: Assertion
-test_abundanceOf12 = True @=? abundant 12
+case_abundanceOf12 :: Assertion
+case_abundanceOf12 = True @=? abundant 12
 
 -- from https://oeis.org/A005101
 first20Abundants :: [Integer]
@@ -22,8 +21,8 @@ first20Abundants = [ 12, 18, 20, 24, 30, 36, 40, 42, 48, 54
                    , 56, 60, 66, 70, 72, 78, 80, 84, 88, 90
                    ]
 
-test_first20Abundants :: Assertion
-test_first20Abundants = first20Abundants @=? take 20 abundants
+case_first20Abundants :: Assertion
+case_first20Abundants = first20Abundants @=? take 20 abundants
 
 leastSumsOfAbundants :: [Integer]
 leastSumsOfAbundants = [ 24, 30, 32, 36, 38, 40, 42, 44, 48, 50
